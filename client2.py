b2322e787d6082e5cf3a8b5354d3435262e49bcf
Steven Cox <scox@renci.org>
2019-03-06 11:35:24 -0500

    graph diff alpha

--- client2.py
+++ client2.py
@@ -11,8 +11,10 @@ del proto_rtx['knowledge_graph']['edges'][-1]
 proto_rtx2 = copy.deepcopy(test_rtx.answer)
 del proto_rtx2['knowledge_graph']['edges'][-2]
 
+#url = "http://localhost:9999/compare_answers"
+url = "https://compare-kg.renci.org/compare_answers"
 response = requests.post (
-    "http://localhost:9999/compare_answers",
+    url,
     json = {
         "answer_1" : proto_rtx2, #test_rtx.answer,
         "answer_2" : proto_rtx

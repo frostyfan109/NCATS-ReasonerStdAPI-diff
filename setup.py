e4d08f06daeff929e507fe0f1935819f9ae3e636
Deepak Unni <deepak.unni3@gmail.com>
2019-06-10 16:44:03 -0700

    Revert "Add setup.py"
    Unintended commit
    
    This reverts commit 910db68a575f54cfbed5ea382c55a2e17033965d.

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='NCATS-ReasonerStdAPI-diff',
-      version='0.0.1',
-      description='Code to diff between answers from different Reasoners',
-      url='http://github.com/deepakunni3/NCATS-ReasonerStdAPI-diff',
-      author='Deepak Unni',
-      author_email='deepak.unni3@gmail.com',
-      zip_safe=False)
